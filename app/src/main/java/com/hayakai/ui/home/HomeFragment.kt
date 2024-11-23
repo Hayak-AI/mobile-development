70db35bed75354fdce0170672da07838566f6921
Jordi Irawan <jordirwn@gmail.com>
2024-11-23 15:24:31 +0700

    feat: create settings and profile page

--- app/src/main/java/com/hayakai/ui/home/HomeFragment.kt
+++ app/src/main/java/com/hayakai/ui/home/HomeFragment.kt
@@ -1,13 +1,16 @@
 package com.hayakai.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.hayakai.R
 import com.hayakai.databinding.FragmentHomeBinding
+import com.hayakai.ui.profile.ProfileActivity
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentHomeBinding? = null
 
@@ -21,11 +24,26 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        setupAction()
+
         return root
+    }
+
+    private fun setupAction() {
+        binding.btnProfile.setOnClickListener(this)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_profile -> {
+                val intent = Intent(requireContext(), ProfileActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/hayakai/ui/profile/ProfileActivity.kt
@@ -0,0 +1,41 @@
+package com.hayakai.ui.profile
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.hayakai.R
+import com.hayakai.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        setupAction()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun setupAction() {
+        binding.backButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.back_button -> {
+                finish()
+            }
+        }
+    }
+}
